20f83120c03c329241a38c7213e4dd0b2eefcf41
Jake Ward <jakobtw@uw.edu>
2024-05-30 20:00:37 -0700

    adding more stations and fixed lat lon

--- GRAPES_PNW.jl
+++ GRAPES_PNW.jl
@@ -20,16 +20,25 @@ S12 = get_data("FDSN", "UW.QOCL", src="IRIS", s=ts, t=te, detrend=true, rr=false
 S13_station = get_data("FDSN", "UW.MANO", src="IRIS", s=ts, t=te, detrend=true, rr=false, w=true, autoname=true)
 S14 = get_data("FDSN", "UW.OHC", src="IRIS", s=ts, t=te, detrend=true, rr=false, w=true, autoname=true)
 S15_station = get_data("FDSN", "UW.RVW2", src="IRIS", s=ts, t=te, detrend=true, rr=false, w=true, autoname=true)
+S16 = get_data("FDSN", "UW.KIMR", src="IRIS", s=ts, t=te, detrend=true, rr=false, w=true, autoname=true)
+S17 = get_data("FDSN", "UW.TLW1", src="IRIS", s=ts, t=te, detrend=true, rr=false, w=true, autoname=true)
+S18_station = get_data("FDSN", "UW.RATT", src="IRIS", s=ts, t=te, detrend=true, rr=false, w=true, autoname=true)
+
+
+
 
 
 
 #Prune the stations with too many channels for just first 3
 S13 = pull(S13_station, 1:3)
 S15 = pull(S15_station, 1:3)
+S18 = pull(S18_station, 1:3)
+
 
 #Push all the channels into one
 #S = SeisData(S1, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11, S12)
-S_true = SeisData(S1,S2,S3,S4,S5,S6,S7,S8,S9,S10,S11,S12,S13,S14,S15)
+S_true = SeisData(S1,S2,S3,S4,S5,S6,S7,S8,S9,S10,S11,S12,S13,S14,S15,S16,S17,S18,S19)
+
 #Source parameters for M4.6 Roosevelt, WA EQ
 origin_time = DateTime(2019, 7, 12, 9, 51, 38)
 event_location = EQLoc(lat= 47.873, lon= 122.016, dep=28.8)
@@ -38,7 +47,7 @@ sample_time = origin_time + Second(6) #this is from GRAPES example, not sure how
 #set up the parameters for the GRAPES model
 rawT = 4.0 # second of input window
 predictT= 60.0 # seconds to extract future PGA
-k = 5 # nearest neighbors when i did 20 it gave me (k < Nrows)
+k = 10 # nearest neighbors when i did 20 it gave me (k < Nrows)
 maxdist = 30000.0 # meters (meters from or of what?)
 logpga = true # return try PGA in log10(pga [cm/s^2])
 
@@ -54,7 +63,8 @@ preds = Array{GNNGraph}(undef, N)
 
 #Initate the model
 model = load_GRAPES_model() #Thank you Tim
-
+lon_vals = Array{Vector{Float64}}(undef, 1)
+lat_vals = Array{Vector{Float64}}(undef, 1)
 #For loop that generaltes g (GNNGraph) and predictions for each station
 for ii in 1:N
     sample_time = origin_time + Second(ii*3)
@@ -70,8 +80,9 @@ for ii in 1:N
     )
     input_graphs[ii] = g
     preds[ii] = model(input_graphs[ii])
+    lon_vals = lon
+    lat_vals = lat
 end
-
 #Index predictions like this
 preds[30]
 #Validate Predictions
@@ -96,18 +107,36 @@ poly!(ga, state; strokewidth = 0.7, color=:green, rasterize = 5)
 color_map = ColorSchemes.inferno
 
 # Get the values from preds[whatever index you want to plot]
-pred_values = preds[20].ndata.x
+pred_values = preds[25].ndata.x
 
 # Get the range of pred_values for colorrange
 pred_range = (minimum(pred_values), maximum(pred_values))
 
-#plot station data
-j = 1
-for i in 1:3:length(S_true)
-    x = S_true.loc[i].lon
-    y = S_true.loc[i].lat
-    Makie.scatter!(ga, x, y, color=pred_values[j],colormap=color_map, colorrange=pred_range, markersize=10, marker=:circle, label = "Station", rasterize = 5)
-    j += 1
+#plot prediction data
+for i in 1:length(pred_values)
+    x = lon_vals[i]
+    y = lat_vals[i]
+    Makie.scatter!(ga, x, y, color=pred_values[i],colormap=color_map, colorrange=pred_range, markersize=10, marker=:circle, label = "Station", rasterize = 5)
+end
+fig
+
+#Get the values from input_graphs[whatever index you want to plot]
+real_values = input_graphs[25].gdata.u
+
+# Get the range of real_values for colorrange
+real_range = (minimum(real_values), maximum(real_values))
+
+fig = Figure()
+ga = GeoAxis(
+    fig[1, 1]; 
+    dest = "+proj=comill", title ="GRAPES Real Data")
+    
+poly!(ga, state; strokewidth = 0.7, color=:green, rasterize = 5)
+#plot real data
+for i in 1:length(real_values)
+    x = lon_vals[i]
+    y = lat_vals[i]
+    Makie.scatter!(ga, x, y, color=real_values[i],colormap=color_map, colorrange=real_range, markersize=10, marker=:circle, label = "Station", rasterize = 5)
 end
 fig
 
